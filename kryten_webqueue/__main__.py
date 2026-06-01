63ca5f741f9ce61ea0e17c71bbad6a361bb43169
Grant Robertson <me@grantrobertson.com>
2026-05-31 20:55:30 -0700

    feat: add main() entrypoint, include deploy files in wheel as shared-data; bump to 0.2.0

--- kryten_webqueue/__main__.py
+++ kryten_webqueue/__main__.py
@@ -4,7 +4,13 @@ import uvicorn
 from .config import Config
 from .app import create_app
 
-config_path = os.environ.get("WQ_CONFIG", "/etc/kryten-webqueue/config.json")
-config = Config.from_file(config_path)
-app = create_app(config)
-uvicorn.run(app, host=config.host, port=config.port)
+
+def main():
+    config_path = os.environ.get("WQ_CONFIG", "/etc/kryten-webqueue/config.json")
+    config = Config.from_file(config_path)
+    app = create_app(config)
+    uvicorn.run(app, host=config.host, port=config.port)
+
+
+if __name__ == "__main__":
+    main()
